cfb206ccba931065e16956e09153f4cd541fb153
Tom Halliley <tomh@acm.org>
2016-11-09 13:35:53 -0500

    Hitting an obstacle now continues with subsequent commands

--- src/main/java/com/dl/rover/Main.java
+++ src/main/java/com/dl/rover/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(final String[] args) {
 
         try {
+            System.out.println("------------ Test 1 ---------------------");
+
             Grid grid = new Grid(10, 10);
 
             grid.addObstacle(new Point(3,3));
@@ -24,7 +26,37 @@ public class Main {
                 'r',
                 'f', 'f', // now at (3,2)
                 'f',      // OBSTACLE!
-                'f' // should not happen
+                'r',
+                'b', 'b' // just fine
+            };
+
+            rover.executeCommands(commands, grid);
+
+        } catch (final Exception e) {
+        }
+
+        try {
+            System.out.println("------------ Test 2 ---------------------");
+
+            Grid grid = new Grid(10, 10);
+
+            grid.addObstacle(new Point(3,3));
+            grid.addObstacle(new Point(3,4));
+            grid.addObstacle(new Point(3,5));
+            grid.addObstacle(new Point(3,6));
+            grid.addObstacle(new Point(3,7));
+
+            //DEBUG            grid.showObstacles();
+
+            Rover rover = new Rover(new Point(0,0), FacingDirection.E);
+
+            char[] commands = new char[] {
+                'f', 'f', 'f', // now at (3,0)
+                'l',
+                'f', // wrap .. now at (3,10)
+                'f', 'f',
+                'f',      // OBSTACLE!
+                'f'       // OBSTACLE!
             };
 
             rover.executeCommands(commands, grid);
--- src/main/java/com/dl/rover/domain/Rover.java
+++ src/main/java/com/dl/rover/domain/Rover.java
@@ -28,13 +28,13 @@ public class Rover {
         this.facingDirection = facingDirection;
     }
 
-    public void move(final Grid grid, final MovingDirection movingDirection) throws ObstacleInTheWayException {
+    public void move(final Grid grid, final MovingDirection movingDirection) {
 
         Point newPoint = grid.consideredPoint(getLocation(), facingDirection, movingDirection);
 
         if (grid.hasObstacle(newPoint)) {
             System.out.println("!!! Cannot move " + movingDirection.display() + " from " + getLocation() + " due to obstacle.");
-            throw new ObstacleInTheWayException(newPoint);
+            return;
         }
 
         setLocation(newPoint);
@@ -46,12 +46,12 @@ public class Rover {
     }
 
     public void executeCommands(final char[] commands, final Grid grid)
-        throws InvalidCommandException, ObstacleInTheWayException, OffTheGridException {
+        throws InvalidCommandException, OffTheGridException {
 
         for (char command : commands) {
             MovingDirection movingDirection = MovingDirection.valueOf(command);
 
-            System.out.println("Attempting to move " + movingDirection.display() + " from " + getLocation() + " while facing " + getFacingDirection());
+            System.out.println("Attempting to move " + movingDirection.display() + " from " + getLocation() + " while facing " + getFacingDirection().display());
 
             switch (movingDirection) {
             case F:
@@ -64,7 +64,7 @@ public class Rover {
                 break;
             }
 
-            System.out.println("Now at " + getLocation() + " facing " + getFacingDirection());
+            System.out.println("Now at " + getLocation() + " facing " + getFacingDirection().display());
         }
 
     }
